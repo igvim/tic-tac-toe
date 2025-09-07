5959bb18a80915c345d210f45098a1fb55782cec
Isaiah <116988172+igvim@users.noreply.github.com>
2025-09-07 16:34:56 -0400

    successful first winCheck

--- index.js
+++ index.js
@@ -13,22 +13,65 @@ const gameBoard = (() => {
   
   const getBoard = () => board;
 
-  const getCell = (row, column) => {
-    const board = gameBoard.getBoard();
-    const cell = board[row][column];
-    return cell;
-  };
-
   const winCheck = () => {
-    const board = gameBoard.getBoard();
+
+    const getCell = (row, column) => {
+      const board = gameBoard.getBoard();
+      const cell = board[row][column];
+      return cell;
+    };
+
+    let cell = getCell(1,1);
+
+    const winMessage = () => { console.log('winner') };
+    switch (cell) {
+      case getCell(0,0):
+        if (cell == getCell(2,2)) winMessage();
+        break;
+      case getCell(0,1):
+        if (cell == getCell(2,1)) winMessage();
+        break;
+      case getCell(0,2):
+        if (cell == getCell(2,0)) winMessage();
+        break;
+      case getCell(1,0):
+        if (cell == getCell(1,2)) winMessage();
+        break;
+      default:
+        cell = getCell(0,0);
+    };
+
+    switch (cell) {
+      case getCell(0,1):
+        if (cell == getCell(0,2)) winMessage();
+        break;
+      case getCell(1,0):
+        if (cell == getCell(2,0)) winMessage();
+        break;
+      default:
+        cell = getCell(2,2);
+    };
+
+    switch (cell) {
+      case getCell(0,2):
+        if (cell == getCell(1,2)) winMessage();
+        break;
+      case getCell(2,0):
+        if (cell == getCell(2,1)) winMessage();
+        break;
+      default:
+        console.log('no winner');
+    }
+    /*
     for (i = 0; i < board.length; i++){
       for (j = 0; j < board.length; j++){
         console.log('hi');
       };
     };
-  }
+    */
+  };
 
-  return { getBoard, newBoard, getCell, winCheck }
+  return { getBoard, newBoard, winCheck }
 })();
 
 const Player = (mark) => {
