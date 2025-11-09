3da784661db94a0d5968ef8bdb5b49a4988b3956
Isaiah <116988172+igvim@users.noreply.github.com>
2025-11-09 10:11:41 -0500

    moved checks to controller

--- index.js
+++ index.js
@@ -6,114 +6,19 @@ const gameBoard = (() => {
       board[i] = [];
       for (j = 0; j < column; j++) {
         board[i].push("");
-      };
-    };
+      }
+    }
     return board;
   };
 
   const getBoard = () => board;
 
-  const winCheck = () => {
-    const getCell = (row, column) => {
-      const cell = board[row][column];
-      return cell;
-    };
-
-    const winMessage = (winCell) => {
-      switch (winCell) {
-        case PlayerX.mark:
-          console.log("X wins! New game:");
-          break;
-        case PlayerO.mark:
-          console.log("O wins! New game:");
-          break;
-      };
-      GameController.resetGame();
-    };
-
-    const checkOne = () => {
-      const cell = getCell(1, 1);
-
-      switch (cell) {
-        case getCell(0, 0):
-          if (cell === getCell(2, 2)) {
-            winMessage(cell);
-            return true;
-          };
-        case getCell(0, 1):
-          if (cell === getCell(2, 1)) {
-            winMessage(cell);
-            return true;
-          };
-        case getCell(0, 2):
-          if (cell === getCell(2, 0)) {
-            winMessage(cell);
-            return true;
-          };
-        case getCell(1, 0):
-          if (cell === getCell(1, 2)) {
-            winMessage(cell);
-            return true;
-          };
-        default:
-          return false;
-      };
-    };
-
-    const checkTwo = () => {
-      const cell = getCell(0, 0);
-
-      switch (cell) {
-        case getCell(0, 1):
-          if (cell === getCell(0, 2)) {
-            winMessage(cell);
-            return true;
-          };
-        case getCell(1, 0):
-          if (cell === getCell(2, 0)) {
-            winMessage(cell);
-            return true;
-          };
-        default:
-          return false;
-      };
-    };
-
-    const checkThree = () => {
-      const cell = getCell(2, 2);
-
-      switch (cell) {
-        case getCell(0, 2):
-          if (cell === getCell(1, 2)) {
-            winMessage(cell);
-            return true;
-          };
-        case getCell(2, 0):
-          if (cell === getCell(2, 1)) {
-            winMessage(cell);
-            return true;
-          };
-        default:
-          return false;
-      };
-    };
-
-    if (!checkOne()) {
-      if (!checkTwo()) {
-        if (!checkThree()) {
-          console.log('no winner')
-        };
-      };
-    };
+  const getCell = (row, column) => {
+    const cell = board[row][column];
+    return cell;
   };
 
-  const tieCheck = () => {
-    const noMoves = board.some((row) => row.some((cell) => cell === ""));
-    const isTie = !noMoves;
-    return isTie;
-  };
-
-  return { getBoard, newBoard, winCheck, tieCheck };
+  return { getBoard, newBoard, getCell };
 })();
 
 const Player = (mark) => {
@@ -137,7 +42,102 @@ const GameController = (() => {
 
   const resetGame = () => {
     turnCount = 0;
-    return gameBoard.newBoard(3,3);
+    gameBoard.newBoard(3, 3);
+  };
+
+  const winCheck = () => {
+    const winMessage = (winCell) => {
+      switch (winCell) {
+        case PlayerX.mark:
+          console.log("X wins! New game:");
+          break;
+        case PlayerO.mark:
+          console.log("O wins! New game:");
+          break;
+      }
+    };
+
+    const checkOne = () => {
+      const cell = gameBoard.getCell(1, 1);
+      if (!cell) return false;
+      switch (cell) {
+        case gameBoard.getCell(0, 0):
+          if (cell === gameBoard.getCell(2, 2)) {
+            winMessage(cell);
+            return true;
+          }
+        case gameBoard.getCell(0, 1):
+          if (cell === gameBoard.getCell(2, 1)) {
+            winMessage(cell);
+            return true;
+          }
+        case gameBoard.getCell(0, 2):
+          if (cell === gameBoard.getCell(2, 0)) {
+            winMessage(cell);
+            return true;
+          }
+        case gameBoard.getCell(1, 0):
+          if (cell === gameBoard.getCell(1, 2)) {
+            winMessage(cell);
+            return true;
+          }
+        default:
+          return false;
+      }
+    };
+
+    const checkTwo = () => {
+      const cell = gameBoard.getCell(0, 0);
+      if (!cell) return false;
+      switch (cell) {
+        case gameBoard.getCell(0, 1):
+          if (cell === gameBoard.getCell(0, 2)) {
+            winMessage(cell);
+            return true;
+          }
+        case gameBoard.getCell(1, 0):
+          if (cell === gameBoard.getCell(2, 0)) {
+            winMessage(cell);
+            return true;
+          }
+        default:
+          return false;
+      }
+    };
+
+    const checkThree = () => {
+      const cell = gameBoard.getCell(2, 2);
+      if (!cell) return false;
+      switch (cell) {
+        case gameBoard.getCell(0, 2):
+          if (cell === gameBoard.getCell(1, 2)) {
+            winMessage(cell);
+            return true;
+          }
+        case gameBoard.getCell(2, 0):
+          if (cell === gameBoard.getCell(2, 1)) {
+            winMessage(cell);
+            return true;
+          }
+        default:
+          return false;
+      }
+    };
+
+    if (!checkOne()) {
+      if (!checkTwo()) {
+        if (!checkThree()) {
+          console.log("no winner");
+          return false;
+        }
+      }
+    }
+  };
+
+  const tieCheck = () => {
+    const noMoves = board.some((row) => row.some((cell) => cell === ""));
+    const isTie = !noMoves;
+    return isTie;
   };
 
   const turn = (i, j) => {
@@ -147,23 +147,27 @@ const GameController = (() => {
     ++turnCount;
 
     if (turnCount > 4) {
-      const isWin = gameBoard.winCheck();
-      if (isWin) resetGame();
+      winCheck();
     }
 
-    if ((turnCount === board.length) ^ 2) gameBoard.tieCheck();
-    return board;
+    if (turnCount === board.length ** 2) {
+      if (tieCheck()) console.log("tie");
+    }
   };
 
   const checkTest = () => {
-    board = gameBoard.newBoard(3,3);
-    turn(0,0);
-    turn(1,0);
-    turn(1,1);
-    turn(0,1);
-    turn(2,2);
+    board = gameBoard.newBoard(3, 3);
+    turn(0, 0);
+    turn(0, 2);
+    turn(0, 1);
+    turn(1, 1);
+    turn(1, 2);
+    turn(1, 0);
+    turn(2, 0);
+    turn(2, 2);
+    turn(2, 1);
     return board;
-  }
+  };
 
   return { turn, checkTest, resetGame, getTurn };
 })();
